f4ec5a4f2548f4ff277d74edb63d1c6398f98f95
Joel Webber <jgw@pobox.com>
2024-09-09 00:03:27 -0400

    Follow-up from last commit.

--- world/builder.py
+++ world/builder.py
@@ -12,6 +12,7 @@ from agency.tools.browse import Browse
 from agency.tools.notebook import Notebook
 from agency.tools.recipes import Recipes
 from agency.tools.search import Search
+from agency.ui import AgencyUI
 
 TAVILY_API_KEY = os.environ["TAVILY_API_KEY"]
 
@@ -23,8 +24,8 @@ dbclient = chromadb.PersistentClient(work_dir + "/chroma")
 embed_model = TextEmbeddingModel.from_pretrained("text-embedding-004")
 
 tools: List[Tool] = [
-    Recipes(embed_model, dbclient, "recipes", "world/recipes"),
-    Notebook(embed_model, dbclient, work_dir + "/notebook", "world/knowledge"),
+    Recipes(embed_model, dbclient, "world/recipes"),
+    Notebook(embed_model, dbclient, "world/knowledge"),
     Search(TAVILY_API_KEY),
     Browse(),
 ]
@@ -52,4 +53,10 @@ model = GenerativeModel(
     ],
 )
 
-agency = Agency(model, tools)
+
+def run():
+    agency = Agency(model, tools)
+    AgencyUI(agency).run()
+
+
+run()
